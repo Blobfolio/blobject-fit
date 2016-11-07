076ca401390b7a47fbff1ef5fbd4a996bb67ed6c
Federico Brigante <bfred-it@users.noreply.github.com>
2016-11-07 12:10:23 +0700

    Use style.textContent
    - style.type = 'text/css'; is the default value
    - style.textContent is supported in IE9 as the standard way to set its
    content
    
    https://developer.mozilla.org/en/docs/Web/API/Node/textContent#Browser_c
    ompatibility

--- src/blobject-fit.js
+++ src/blobject-fit.js
@@ -73,11 +73,7 @@
 			head = document.head || document.getElementsByTagName('head')[0],
 			style = document.createElement('style');
 
-		style.type = 'text/css';
-		if(style.styleSheet)
-			style.styleSheet.cssText = css;
-		else
-			style.appendChild(document.createTextNode(css));
+		style.textContent = css;
 
 		head.appendChild(style);
 
